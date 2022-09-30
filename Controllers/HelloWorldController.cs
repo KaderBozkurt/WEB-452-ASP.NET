8621a5efdbdcccf3c4c3b97789f193160f53e7f1
W0799338 <W0799338@myscc.ca>
2022-09-30 13:49:42 -0400

    third

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -9,27 +9,35 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-       
+        public IActionResult Index()
+        {
+            return View();
+        }
         // GET: /HelloWorld/
 
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        /* public string Index()
+         {
+             return "This is my default action...";
+         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-        /*
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
-        */
+         // 
+         // GET: /HelloWorld/Welcome/ 
+         /*
+         public string Welcome()
+         {
+             return "This is the Welcome action method...";
+         }
+         */
 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
+
+
     
 }
